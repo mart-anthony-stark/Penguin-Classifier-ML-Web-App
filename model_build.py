29ce8d74aa0dafc6dcc9183894b6c730ec5e85fa
mart-anthony-stark <martsalazar0@gmail.com>
2022-07-11 19:28:39 +0800

    add: model building

--- model_build.py
+++ model_build.py
@@ -4,4 +4,28 @@ penguins = pd.read_csv('penguins_cleaned.csv')
 # Ordinal feature encoding
 df = penguins.copy()
 target = 'species'
-encode = ['sex', 'islands']
+encode = ['sex', 'island']
+
+for col in encode:
+  dummy = pd.get_dummies(df[col], prefix=col)
+  df = pd.concat([df,dummy], axis=1)
+  del df[col]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+  return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
+
+# Separating X and y
+X = df.drop('species', axis=1)
+y = df['species']
+
+# Build random forest model
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
+clf.fit(X, y)
+
+# Save the model
+import pickle
+pickle.dump(clf, open('penguins_clf.pkl', 'wb'))
